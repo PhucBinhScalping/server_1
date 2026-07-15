b54eeaf0dd72a27f93fc60073636c27d69fc8b37
Nguyễn Phúc Bình <nguyenphucbinh8386@gmail.com>
2026-07-15 23:24:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def tinh_du_lieu_cp(symbol):
         
         df = pd.DataFrame(data)
         df['pctChange'] = pd.to_numeric(df['pctChange'], errors='coerce')
-        df['volume'] = pd.to_numeric(df['nmVolume'], errors='coerce').fillna(0) + pd.to_numeric(df['ptVolume'], errors='coerce').fillna(0)
+        df['volume'] = pd.to_numeric(df['nmVolume'], errors='coerce').fillna(0)
         
         # 1. Lọc thanh khoản (TB 100 phiên > 100,000)
         volume_tb100 = df['volume'].tail(100).mean()
@@ -106,7 +106,7 @@ def main():
     ))
     
     fig.update_layout(
-            title=f"Biến động ngành - {vn_now.strftime('%d-%m-%Y')}", 
+            title=f"Biến động ngành - {vn_now.strftime('%d-%m-%Y %H:%M:%S')}", 
             paper_bgcolor='#333333', 
             plot_bgcolor='#333333', 
             font=dict(color='white'),
